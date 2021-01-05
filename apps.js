feb0c22e875f45052c4756ca1aa66e35bbd2a042
shrey27 <shreypandey27@gmail.com>
2021-01-05 17:15:10 +0530

    bug fix

--- apps.js
+++ apps.js
@@ -1,20 +1,38 @@
-var btnPrimary = document.getElementById("btn-primary");//document.querySelector("#btn-primary");
-var textIn = document.querySelector("#textInput");
-var textOut = document.querySelector("#textOutput");
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+// var btnPrimary = document.getElementById("btn-primary");//document.querySelector("#btn-primary");
+// var textIn = document.querySelector("#textInput");
+// var textOut = document.querySelector("#textOutput");
+// var serverURL = "https://api.funtranslations.com/translate/minion.json";
+// function getServerURL(input){
+//     return serverURL+"?"+"text="+input;
+// }
+// function errorHandler(){
+//     alert("Technical fault..!! Please try again after some time.");
+// }
+// function clickHandle(){
+//     var inputText = textIn.value;
+//     fetch(getServerURL(inputText))
+//     .then(response => response.json())
+//     .then(json => {
+//         textOut.innerText = json.contents.translated;
+//     })
+//     .catch(errorHandler)
+// }
+// btnPrimary.addEventListener("click",clickHandle);
+var btnPrimary = document.getElementById("btn-primary");
+var textIn = document.getElementById("textInput");
+var textOut = document.getElementById("textOutput");
+var serverURL = "api";
 function getServerURL(input){
     return serverURL+"?"+"text="+input;
 }
 function errorHandler(){
-    alert("Technical fault..!! Please try again after some time.");
+    alert("Technical fault");
 }
 function clickHandle(){
-    var inputText = textIn.value;
+    var inputText = textIn.Value;
     fetch(getServerURL(inputText))
-    .then(response => response.json())
+    .then(response => response.json)
     .then(json => {
         textOut.innerText = json.contents.translated;
-    })
-    .catch(errorHandler)
+    }).catch(errorHandler)
 }
-btnPrimary.addEventListener("click",clickHandle);
