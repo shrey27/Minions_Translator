64356bd0c6808f0e0c0e9e719a6f99fd204d71e9
shrey27 <shreypandey27@gmail.com>
2021-01-05 16:54:29 +0530

    code update

--- apps.js
+++ apps.js
@@ -1,10 +1,9 @@
-var btnPrimary = document.querySelector("#btn-primary");
+var btnPrimary = document.getElementById("btn-primary");//document.querySelector("#btn-primary");
 var textIn = document.querySelector("#textInput");
 var textOut = document.querySelector("#textOutput");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 function getServerURL(input){
     return serverURL+"?"+"text="+input;
-
 }
 function errorHandler(){
     alert("Technical fault..!! Please try again after some time.");
