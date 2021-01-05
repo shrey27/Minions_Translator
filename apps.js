98baf7f51a731a2fc657808e756231c60e0bb20c
shrey27 <shreypandey27@gmail.com>
2021-01-05 17:56:25 +0530

    code redo

--- apps.js
+++ apps.js
@@ -1,42 +1,43 @@
-// var btnPrimary = document.getElementById("btn-primary");//document.querySelector("#btn-primary");
-// var textIn = document.querySelector("#textInput");
-// var textOut = document.querySelector("#textOutput");
-// var serverURL = "https://api.funtranslations.com/translate/minion.json";
-// function getServerURL(input){
-//     return serverURL+"?"+"text="+input;
-// }
-// function errorHandler(){
-//     alert("Technical fault..!! Please try again after some time.");
-// }
-// function clickHandle(){
-//     var inputText = textIn.value;
-//     fetch(getServerURL(inputText))
-//     .then(response => response.json())
-//     .then(json => {
-//         textOut.innerText = json.contents.translated;
-//     })
-//     .catch(errorHandler)
-// }
-// btnPrimary.addEventListener("click",clickHandle);
-var btnPrimary = document.getElementById("btn-primary");
-var textIn = document.getElementById("textInput");
-var textOut = document.getElementById("textOutput");
+var btnPrimary = document.getElementById("btn-primary");//document.querySelector("#btn-primary");
+var textIn = document.querySelector("#textInput");
+var textOut = document.querySelector("#textOutput");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
-
 function getServerURL(input){
     return serverURL+"?"+"text="+input;
 }
-
 function errorHandler(){
-    alert("Technical fault");
+    alert("Technical fault..!! Please try again after some time.");
 }
-
 function clickHandle(){
-    var inputText = textIn.Value;
+    var inputText = textIn.value;
     fetch(getServerURL(inputText))
     .then(response => response.json())
     .then(json => {
         textOut.innerText = json.contents.translated;
-    }).catch(errorHandler)
+    })
+    .catch(errorHandler)
 }
 btnPrimary.addEventListener("click",clickHandle);
+
+//Redo code
+// ******************************************************* //
+// var btnPrimary = document.getElementById("btn-primary");
+// var textInput = document.getElementById("textInput");
+// var textOutput = document.getElementById("textOutput");
+// var url = "https://api.funtranslations.com/translate/minion.json";
+// function getServerURL(input){
+//     return url+"?"+"text="+input;
+// }
+// function errorHandler(){
+//     alert("technical fault occurred");
+// }
+// function clickHandle(){
+//     var inputText = textInput.value;
+//     fetch(getServerURL(inputText))
+//     .then(response=>response.json())
+//     .then(json => {
+//         textOutput.innerText = json.contents.translated;
+//     })
+//     .catch(errorHandler);
+// }
+// btnPrimary.addEventListener("click",clickHandle);
